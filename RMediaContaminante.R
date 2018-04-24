7559c645bf7b7f0e583ecdf79237ee8b430b0ff9
AlfredoCentell <35700789+AlfredoCentell@users.noreply.github.com>
2018-04-24 18:15:02 -0500

    Add files via upload

---
+++ RMediaContaminante.R
@@ -0,0 +1,40 @@
+
+
+mediacontaminante <- function(directorio, contaminante, id){
+  
+  setwd(directorio)
+  
+  name <- paste("Monitor", substr(list.files()[1],start = 1,stop = 3))
+  c <- read.csv(list.files()[1])[contaminante] #contaminante en monitor i(1)
+  mc <- as.matrix(mean(c[!is.na(c)])) #media del contaminante en monitor i(1)
+  rownames(mc)[1] <- name
+  
+  for (i in 2:332){
+    
+    name <- paste("Monitor", substr(list.files()[i],start = 1,stop = 3))
+    c <- read.csv(list.files()[i])[contaminante]
+    mc2 <- as.matrix(mean(c[!is.na(c)]))
+    
+    if (!is.nan(mc2) == TRUE){
+      rownames(mc2)[1] <- name
+      mc <- rbind(mc, mc2) #media de n monitores
+    }else{
+      mc2 <- paste("Any value of", contaminante, "registered")
+      mc2 <- as.matrix(mc2)
+      rownames(mc2)[1] <- name
+      mc <- rbind(mc, mc2)
+      
+    }
+  }
+  
+  mc <- mc[id,]
+  mc <- as.matrix(mc)
+  colnames(mc) <- contaminante
+  mc
+} 
+
+x <- "C:\Users\alfre\OneDrive\Documentos\GitHub\Software Actuarial III"
+y <- "sulfate" #sulfate|nitrate
+z <- 1:10 #332
+
+mediacontaminante(x,y,z)
---
+++ Rcompletos.R
@@ -0,0 +1,37 @@
+
+completos <- function(directorio, id){
+  
+  setwd(directorio)
+  
+  name <- paste("Monitor", substr(list.files()[1],start = 1,stop = 3))
+  a <- read.csv(list.files()[1]) #monitor i(1)
+  a <- (!is.na(a$sulfate)) & (!is.na(a$nitrate)) #casos completos
+  a <- sum(a) #nobs
+  nobs <- data.frame(matrix(c(name, a), nrow = 1)) #se convierte en dataframe
+  
+  for (i in 2:332){
+    
+    name <- paste("Monitor", substr(list.files()[i],start = 1,stop = 3))
+    a <- read.csv(list.files()[i]) #monitor i(1)
+    a <- (!is.na(a$sulfate)) & (!is.na(a$nitrate)) #casos completos
+    a <- sum(a)
+    nobs2 <- data.frame(matrix(c(name, a), nrow = 1))
+    
+    nobs <- rbind(nobs, nobs2) #unión de data frames
+    
+  }
+  
+  nobs <- nobs[id, ]
+  colnames(nobs)[1] = "id"
+  colnames(nobs)[2] = "nobs"
+  nobs
+  
+} 
+
+x <- "C:\Users\alfre\OneDrive\Documentos\GitHub\Software Actuarial III"
+y = 1:10 #332
+
+completos(x,y)
+
+xx <- c(1:100)
+xx[c(12:30)]
