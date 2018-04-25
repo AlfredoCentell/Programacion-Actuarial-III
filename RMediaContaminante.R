fe3b53f506e9d3ddf522c0f7d03dcfa8521136ea
AlfredoCentell <35700789+AlfredoCentell@users.noreply.github.com>
2018-04-25 10:55:15 -0500

    Update RMediaContaminante.R

--- RMediaContaminante.R
+++ RMediaContaminante.R
@@ -1,25 +1,23 @@
 
 
-medicont <- function(directorio, contaminante, id){
+medicont <- function(directory, contaminante, id){
   
-  setwd(directorio)
+  setwd(directory)
   name <- paste("Monitor", substr(list.files()[1],start = 1,stop = 3))
   c <- read.csv(list.files()[1])[contaminante]
   mc <- as.matrix(mean(c[!is.na(c)])) 
   rownames(mc)[1] <- name
-  
-  for (i in 2:332){
+   for (i in 2:332){
      name <- paste("Monitor", substr(list.files()[i],start = 1,stop = 3))
-    c <- read.csv(list.files()[i])[contaminante]
+    c <- read.csv(list.files())
     mc2 <- as.matrix(mean(c[!is.na(c)]))
     
     if (!is.nan(mc2) == TRUE){
       rownames(mc2)[1] <- name
       mc <- rbind(mc, mc2)
-    }else{
+    }else
       mc2 <- paste("Any value of", contaminante, "registered")
       mc2 <- as.matrix(mc2)
-      rownames(mc2)[1] <- name
       mc <- rbind(mc, mc2)
       
     }
@@ -27,11 +25,9 @@ medicont <- function(directorio, contaminante, id){
   
   mc <- mc[id,]
   mc <- as.matrix(mc)
-  colnames(mc) <- contaminante
-  mc
 } 
 
 x <- "C:\Users\alfre\OneDrive\Documentos\GitHub\Software Actuarial III"
 y <- "sulfate"
-z <- 1:10 
+z <- 1:12
 medicont(x,y,z)
