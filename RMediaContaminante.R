60c68c3215e6829b89d5284d1e27447e216389ce
AlfredoCentell <35700789+AlfredoCentell@users.noreply.github.com>
2018-04-25 10:48:03 -0500

    Update RMediaContaminante.R

--- RMediaContaminante.R
+++ RMediaContaminante.R
@@ -1,23 +1,21 @@
 
 
-mediacontaminante <- function(directorio, contaminante, id){
+medicont <- function(directorio, contaminante, id){
   
   setwd(directorio)
-  
   name <- paste("Monitor", substr(list.files()[1],start = 1,stop = 3))
-  c <- read.csv(list.files()[1])[contaminante] #contaminante en monitor i(1)
-  mc <- as.matrix(mean(c[!is.na(c)])) #media del contaminante en monitor i(1)
+  c <- read.csv(list.files()[1])[contaminante]
+  mc <- as.matrix(mean(c[!is.na(c)])) 
   rownames(mc)[1] <- name
   
   for (i in 2:332){
-    
-    name <- paste("Monitor", substr(list.files()[i],start = 1,stop = 3))
+     name <- paste("Monitor", substr(list.files()[i],start = 1,stop = 3))
     c <- read.csv(list.files()[i])[contaminante]
     mc2 <- as.matrix(mean(c[!is.na(c)]))
     
     if (!is.nan(mc2) == TRUE){
       rownames(mc2)[1] <- name
-      mc <- rbind(mc, mc2) #media de n monitores
+      mc <- rbind(mc, mc2)
     }else{
       mc2 <- paste("Any value of", contaminante, "registered")
       mc2 <- as.matrix(mc2)
@@ -34,7 +32,6 @@ mediacontaminante <- function(directorio, contaminante, id){
 } 
 
 x <- "C:\Users\alfre\OneDrive\Documentos\GitHub\Software Actuarial III"
-y <- "sulfate" #sulfate|nitrate
-z <- 1:10 #332
-
-mediacontaminante(x,y,z)
+y <- "sulfate"
+z <- 1:10 
+medicont(x,y,z)
