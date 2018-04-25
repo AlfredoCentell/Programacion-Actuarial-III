430660f3c0020c5b18cfaee728cbad8aba1feb12
AlfredoCentell <35700789+AlfredoCentell@users.noreply.github.com>
2018-04-25 10:58:16 -0500

    Update Rcompletos.R

--- Rcompletos.R
+++ Rcompletos.R
@@ -1,9 +1,9 @@
 
-comp <- function(directorio, id){
+comp <- function(directory, id){
   
-  setwd(directorio)
+  setwd(directory)
   name <- paste("Monitor", substr(list.files()[1],start = 1,stop = 3))
-  a <- read.csv(list.files()[1]) #monitor i(1)
+  a <- read.csv(list.files()[1]) 
   a <- (!is.na(a$sulfate)) & (!is.na(a$nitrate)) 
   a <- sum(a) 
   nobs <- data.frame(matrix(c(name, a), nrow = 1)) 
@@ -11,18 +11,14 @@ comp <- function(directorio, id){
     name <- paste("Monitor", substr(list.files()[i],start = 1,stop = 3))
     a <- read.csv(list.files()[i]) 
     a <- (!is.na(a$sulfate)
-    a <- sum(a)
     nobs2 <- data.frame(matrix(c(name, a), nrow = 1))
-    
-    nobs <- rbind(nobs, nobs2) 
     }
   nobs <- nobs[id, ]
   colnames(nobs)[1] = "id"
   colnames(nobs)[2] = "nobs"
-  nobs
   } 
 x <- "C:\Users\alfre\OneDrive\Documentos\GitHub\Software Actuarial III"
-y = 1:10 #332
+y = 1:10 
 comp(x,y)
 
 
