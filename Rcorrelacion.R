97a633d988623c1f7cc1df7c1635215dbb14a9f9
AlfredoCentell <35700789+AlfredoCentell@users.noreply.github.com>
2018-04-25 11:02:25 -0500

    Update Rcorrelacion.R

--- Rcorrelacion.R
+++ Rcorrelacion.R
@@ -1,35 +1,23 @@
 
-corr <- function(directorio, horizonte){
+correlacion <- function(directory, horizonte){
   
-  setwd(directorio)
-  
+setwd(directory)
   if(horizonte == 0){
-    
-    c(0)
-    
-  } else {
+    } else {
     total <- Inf - Inf
     for(i in 1:332){
-      
-      name <- paste("Monitor", substr(list.files()[i],start = 1,stop = 3))
-      a <- read.csv(list.files()[i]) #monitor i(1)
-      a <- (!is.na(a$sulfate)) & (!is.na(a$nitrate)) #casos completos
-      h <- sum(a) #horizonte
-      
-      if(h>=horizonte && is.nan(total)){
-        
-        s <- read.csv(list.files()[i])[a, ][ ,"sulfate"]
-        n <- read.csv(list.files()[i])[a, ][ ,"nitrate"]
-        a <- cor(s,n) #
+       name <- paste("Monitor", substr(list.files()[i],start = 1,stop = 3))
+      a <- read.csv(list.files()[i])
+      a <- (!is.na(a$sulfate)) & (!is.na(a$nitrate))  
+       if(h>=horizonte && is.nan(total)){
+        s <- read.csv(list.files()[i])[a, ]
+        n <- read.csv(list.files()[i])[a, ]
+        a <- cor(s,n) 
         names(a)[1] = name
         total <- a
-        names(total)[length(total)] = name
-        
-      }else if(h>=horizonte){
-        
+         } else if(h>=horizonte){
         s <- read.csv(list.files()[i])[a, ][ ,"sulfate"]
         n <- read.csv(list.files()[i])[a, ][ ,"nitrate"]
-        a <- cor(s,n) #
         total <- c(total,a)
         names(total)[length(total)] = name
         
@@ -46,4 +34,4 @@ corr <- function(directorio, horizonte){
 x <- "C:\Users\alfre\OneDrive\Documentos\GitHub\Software Actuarial III"
 y = 0
 
-corr(x,y)
+correlacion(x,y)
