6a29195c357c20fa7a13fe60c07f3a62361556a7
AlfredoCentell <35700789+AlfredoCentell@users.noreply.github.com>
2018-05-21 17:01:40 -0500

    Update rankingcompleto.R

--- rankingcompleto.R
+++ rankingcompleto.R
@@ -3,30 +3,68 @@ getwd()
 setwd("C:/Users/alfre/OneDrive/Documentos/GitHub/Software Actuarial III/Caso 2")
 
 rankingcompleto <- function(resultado, num = "mejor") {
-
-outcome <- read.csv("outcome-of-care-measures.csv")        
   
+data <- read.csv("outcome-of-care-measures.csv")
+  
+  if (!((resultado == "ataque") | (resultado == "falla")
+        | (resultado == "neumonia"))) {
+    stop ("resultado invalido")
+  }
+
 # Los numeros(11,17 y 23) representan las columnas donde se encuentran los datos
 # de ese caso
+# Para cada estado, encuentra el hospital con la posición cada.
+
+  col <- if (resultado == "ataque") {
+    11 
+  } else if (resultado == "falla") {
+    17 
+  } else {
+    23 
+  }
   
-if (resultado == "ataque"){      
+data[, col] <- suppressWarnings(as.numeric(levels(data[, col])[data[, col]]))
+
+data[, 2] <- as.character(data[, 2])
+  
+resultadoFin <- vector() #Vector vacío 
+
+estados <- levels(data[, 7])
+  
+for(i in 1:length(estados)) {
+  DataEstado <- data[grep(estados[i], data$State), ]
+  Ordenar_Data <- DataEstado[order(DataEstado[, col], DataEstado[, 2], na.last = NA), ]
     
-  resultado <- 11
+   hospital <- if(num == "mejor") {
+  Ordenar_Data[1, 2]
+  } else if(num == "peor") {
+   Ordenar_Data[nrow(Ordenar_Data), 2]
+  } else{
+      Ordenar_Data[num, 2]
+ }
     
-} else if (resultado == "falla") {
+  resultadoFin <- append(resultadoFin, c(hospital, estados[i])) 
     
-  resultado <- 17
-    
-} else if (resultado == "neumonia"){
-    
-  resultado <- 23
-    
-} else {
-    
-  stop("Resultado invalido")          # si es TRUE la funcion se detiene aqui
+  # append agrega elementos al vector que teniamos vacio al inincio
+  
+  }
+  
+  # Regresa un data frame con el nombre del hospital y la abreviatura
+  # del nombre del estado al que pertenece.
+  
+  resultadoFin <- as.data.frame(matrix(resultadoFin, length(estados), 2, byrow = TRUE))
+  
+  colnames(resultadoFin) <- c("hospital", "estado")
+  
+  rownames(resultadoFin) <- estados
+  
+  resultadoFin
+  
 }
 
-# Aqui no se revisa el estado porque aplica para todos los estados
+head(rankingcompleto("ataque",20),10)
+
+tail(rankingcompleto("neumonia","peor"),3)      # para correr selecciono todo y asi jala
   
   
   
